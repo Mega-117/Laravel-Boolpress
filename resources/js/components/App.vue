ea28ee15a6373813821922ce773dac039ddb37e5
Mega-117 <enricosaian@hotmail.it>
2022-01-17 21:25:22 +0100

    correzioni

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      messaggio: "Benvenuto ;)",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+//Route::get('admin/home', 'admin/HomeController@index')->name('admin.home');
+
+Route::namespace("Admin")
+    ->name("admin.")
+    ->prefix("admin")
+    ->middleware("auth")
+    ->group(function(){
+    
+    Route::get('/', 'HomeController@index')->name('home');
+});
+
+Route::get("{any?}", function(){
+    return view("guests.home");
+})->where("any", ".*");
