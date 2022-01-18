fad92d02e82d226210bf8f331653fb7959ea0041
Mega-117 <enricosaian@hotmail.it>
2022-01-18 18:15:21 +0100

    crud completa

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -6,17 +6,18 @@
         <h1>{{ $post['title'] }}</h1>
         <p>{{ $post['text'] }}</p>
         <br>
-        <a href="{{ route('admin.posts.edit', $post->id) }}">Modifica</a>
+        <div class="d-flex">
+
+            <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post->id) }}">Modifica</a>
+            
+            <form action="{{ route('admin.posts.destroy', $post->id) }}" 
+                method="post">
+                @csrf
+                @method('delete') 
+                <input class="btn btn-danger" type="submit" value="Elimina">            
+            </form>
+        </div>
         <br>
         <a href="{{ route('admin.posts.index') }}">Torna alla lista</a>
-
-        <form action="{{ route('admin.posts.destroy', $post->id) }}" 
-            method="post">
-           @csrf
-           @method('delete') 
-
-            <input class="btn btn-danger" type="submit" value="Elimina">
-            
-        </form>
     </div>
 @endsection
