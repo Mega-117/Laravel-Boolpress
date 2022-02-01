c78f32ae4d93c29a21fb30bacc04f965982126fa
Mega-117 <enricosaian@hotmail.it>
2022-02-01 08:32:53 +0100

    passato dati alla view della mail

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -9,7 +9,8 @@ class ContactsController extends Controller
 {
     public function store(Request $request) {
         $data = $request->all();
-        dd($data);
+        //dd($data);
+        
         Mail::to($data['mail'])->send(new SendContactMail($data));
     }
 }
--- app/Mail/SendContactMail.php
+++ app/Mail/SendContactMail.php
@@ -28,6 +28,6 @@ class SendContactMail extends Mailable
      */
     public function build()
     {
-        return $this->view('mail.sendContactMail');
+        return $this->view("mail.sendContactMail");
     }
 }
